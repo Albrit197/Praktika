a982e28805ad5bc4ee551bcd5cd6539121f7c8bb
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-05-06 16:13:18 +0300

    Uppercase

---
+++ js/darom.js
@@ -0,0 +1,34 @@
+ "use strict"
+
+// String.prototype.firstLetter = function () {
+//     return this[0];
+// }
+
+// console.log( 'ajfbkasd'.firstLetter() );
+
+String.prototype.toAlternatingCase = function(){
+    console.log(this);
+    let naujasTekstas = '';
+
+
+    for(let i=0; i<this.length; i++){
+        let naujaRaide = this[i];
+        
+
+        if(naujaRaide === naujaRaide.toLowerCase()){
+             //jeigu raide yra mazoji
+             naujasTekstas = naujasTekstas + naujaRaide.toUpperCase();
+        }
+            else{
+                naujasTekstas = naujasTekstas + naujaRaide.toLowerCase();
+            }
+           
+    }
+
+    console.log(naujasTekstas);
+    return naujasTekstas;
+    
+}
+    //console.log("hello world".toAlternatingCase() === "HELLO WORLD");
+    console.log("Koja".toAlternatingCase() === "kOJA");
+    
